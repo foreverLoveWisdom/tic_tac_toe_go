7bdaf9bd243c1323b3abeaec06229b67367f198c
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-09-20 21:12:14 +0700

    feat: Add function to display the game board

--- main.go
+++ main.go
@@ -1,17 +1,45 @@
 package main
 
-import "log"
+import (
+	"log"
+	"strings"
+)
 
 func InitializeBoard() [3][3]rune {
 	var board [3][3]rune
 
-	for row := 0; row < 3; row++ {
-		for col := 0; col < 3; col++ {
+	for row := range [3]struct{}{} {
+		for col := range [3]struct{}{} {
 			board[row][col] = ' '
 		}
-
 	}
+
 	return board
+}
+
+func DisplayBoard(board [3][3]rune) string {
+	var sb strings.Builder
+
+	for row := range [3]struct{}{} {
+		for col := range [3]struct{}{} {
+			sb.WriteRune(' ')
+			sb.WriteRune(board[row][col])
+			const maxCol = 2
+
+			if col < maxCol {
+				sb.WriteString(" |")
+			}
+		}
+
+		sb.WriteString("\n")
+		const maxRow = 2
+
+		if row < maxRow {
+			sb.WriteString("---+---+---\n")
+		}
+	}
+
+	return sb.String()
 }
 
 func main() {
