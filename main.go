34f3da54175a9804b6643557eb480dd3984e9bd8
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-09-20 20:54:52 +0700

    feat: Add function to initialize game board

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import "log"
+
+func InitializeBoard() [3][3]rune {
+	var board [3][3]rune
+
+	for row := 0; row < 3; row++ {
+		for col := 0; col < 3; col++ {
+			board[row][col] = ' '
+		}
+
+	}
+	return board
+}
+
+func main() {
+	board := InitializeBoard()
+	log.Println(board)
+}
