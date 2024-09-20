137e72f89ba073f03c9409bb9fd20a63348f1db8
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-09-20 21:11:55 +0700

    test: Add test for displaying the game board

--- main_test.go
+++ main_test.go
@@ -12,3 +12,15 @@ func TestInitialBoard(t *testing.T) {
 		}
 	}
 }
+
+func TestDisplayBoard(t *testing.T) {
+	board := InitializeBoard()
+	board[0][0] = 'X'
+	board[1][1] = 'O'
+	expectedOutput := " X |   |  \n---+---+---\n   | O |  \n---+---+---\n   |   |  \n"
+
+	got := DisplayBoard(board)
+	if got != expectedOutput {
+		t.Errorf("DisplayBoard() =\n%s\nExpected:\n%s", got, expectedOutput)
+	}
+}
