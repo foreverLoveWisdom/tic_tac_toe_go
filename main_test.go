c08ab0b5fa73ab73b58916a54bbdfe18ed627d74
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-09-20 20:54:18 +0700

    test: Added test for initial board setup

--- main_test.go
+++ main_test.go
@@ -1,3 +1,14 @@
 package main
 
+import "testing"
 
+func TestInitialBoard(t *testing.T) {
+	board := InitializeBoard()
+	for row := range [3]int{} {
+		for col := range [3]int{} {
+			if board[row][col] != ' ' {
+				t.Errorf("Expected cell (%d,%d) to be empty, got %c", row, col, board[row][col])
+			}
+		}
+	}
+}
