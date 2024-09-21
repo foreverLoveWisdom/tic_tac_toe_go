68f6050e665fee39265bc8155321a14a6a90fcd0
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-09-21 13:18:30 +0700

    feat: Add functions for validating moves, applying moves, and checking for a win

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"errors"
 	"strings"
 )
 
@@ -42,7 +42,54 @@ func DisplayBoard(board [3][3]rune) string {
 	return sb.String()
 }
 
+func IsValidMove(board [3][3]rune, currentRow int, currentCol int) bool {
+	if currentRow < 0 || currentRow >= 3 || currentCol < 0 || currentCol >= 3 {
+		return false
+	}
+
+	return board[currentRow][currentCol] == ' '
+}
+
+func ApplyMove(board [3][3]rune, currentRow int, currentCol int, player rune) ([3][3]rune, error) {
+	if player != 'X' && player != 'O' {
+		// return board, fmt.Errorf("invalid player: %c", player)
+		return board, errors.New("invalid player")
+	}
+
+	if !IsValidMove(board, currentRow, currentCol) {
+		return board, errors.New("invalid move")
+	}
+
+	newBoard := board
+	newBoard[currentRow][currentCol] = player
+
+	return newBoard, nil
+}
+
+func CheckWin(board [3][3]rune, player rune) bool {
+	for row := range [3]struct{}{} {
+		if board[row][0] == player && board[row][1] == player && board[row][2] == player {
+			return true
+		}
+	}
+
+	for col := range [3]struct{}{} {
+		if board[0][col] == player && board[1][col] == player && board[2][col] == player {
+			return true
+		}
+	}
+
+	if board[0][0] == player && board[1][1] == player && board[2][2] == player {
+		return true
+	}
+	if board[0][2] == player && board[1][1] == player && board[2][0] == player {
+		return true
+	}
+
+	return false
+}
+
 func main() {
 	board := InitializeBoard()
-	log.Println(board)
+	DisplayBoard(board)
 }
